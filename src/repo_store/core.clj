649f598df0794e06582f7cd46b53c33d03ce5903
Joe Moon <joe@xoxomoon.com>
2015-01-19 22:28:26 -0800

    strip extensions from key

--- src/repo_store/core.clj
+++ src/repo_store/core.clj
@@ -1,5 +1,6 @@
 (ns repo-store.core
-  (:require [repo-store.config :as config]
+  (:require [clojure.string :as string]
+            [repo-store.config :as config]
             [repo-store.repo :as repo]
             [repo-store.parse :as parse]))
 
@@ -11,14 +12,24 @@
 (def filter-markdown-files (partial filter (partial re-matches #".*\.(md|markdown)$")))
 
 (defn get-contents [file-name]
-  (slurp (str config/path-prefix config/repo "/" file-name)))
+  (->> file-name
+    (str config/path-prefix config/repo "/")
+    (slurp)))
+
+(def strip-ext #(as-> % s
+                  (string/split s #"\.")
+                  (drop-last s)
+                  (string/join "." s)))
 
 (def change-set (repo/get-change-set config/repo))
 (def added (change-set :add))
 (def added-markdown-files (filter-markdown-files added))
 
 (pprint (into {} (map
-           #(vector % (-> %
-                          get-contents
-                          parse/parse))
-           added-markdown-files)))
+                   #(vector
+                      (strip-ext %)
+                      (-> %
+                          (get-contents)
+                          (parse/parse)
+                          (assoc :filename %)))
+                   added-markdown-files)))
