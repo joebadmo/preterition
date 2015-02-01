79fa24d010c3e0405e004830136e985e48a72140
Joe Moon <joe@xoxomoon.com>
2015-02-01 00:04:46 -0800

    working json api

--- src/repo_store/web.clj
+++ src/repo_store/web.clj
@@ -3,9 +3,28 @@
             [compojure.route :as route]
             [clojure.string :refer [split join]]
             [repo-store.core :refer [on-post]]
-            [repo-store.database :as db]))
+            [repo-store.database :as db]
+            [ring.middleware.json :refer [wrap-json-response]]))
 
-(defroutes app
-  (GET "/" [] "hi")
+(def not-found {:status 404 :body "Not found"})
+
+(def get-document
+  (GET "/*" {uri :uri}
+       (if-let [doc (-> (get-path uri) (db/select-document))]
+         {:body doc}
+         not-found)))
+
+(defroutes app-routes
   (POST "/repo/:username/:repo" [username repo]
-        (on-post (str username "/" repo))))
+        (on-post (str username "/" repo)))
+  (GET "/documents" []
+       {:body (db/select-documents)})
+  (context "/document" [] get-document))
+
+(def app (-> app-routes
+             wrap-json-response))
+
+(defn get-path [uri]
+  (->> (split uri #"\/")
+       (drop 2)
+       (join "/")))
