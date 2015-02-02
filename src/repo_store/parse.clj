c89255ff10bf5d42e89530abb6c289279f9bfec5
Joe Moon <joe@xoxomoon.com>
2015-02-01 23:43:32 -0800

    added blog aliases

--- src/repo_store/parse.clj
+++ src/repo_store/parse.clj
@@ -9,29 +9,27 @@
        (drop-last)
        (join ".")))
 
-(defn parse-post [full-filename]
-  (let [path-tokens (split full-filename #"\/")
+(defn parse-post [document file-path]
+  (let [path-tokens (split file-path #"\/")
         dir (->> (drop-last path-tokens) (join "/"))
         filename (last path-tokens)
         filename-tokens (split filename #"-")
-        date-tokens (take 3 filename-tokens)]
-    {:date (->> date-tokens
-                (map #(Integer. %))
-                (apply t/date-time))
-     :path (->> filename-tokens
-                (drop 3)
-                (join "-")
-                (str dir "/" (join "/" date-tokens) "/")
-                (strip-ext))}))
+        date-tokens (take 3 filename-tokens)
+        slug (->> (drop 3 filename-tokens) (join "-") (strip-ext))]
+    (-> document
+        (merge {:date (->> date-tokens
+                           (map #(Integer. %))
+                           (apply t/date-time))
+                :path (->> (concat [dir] date-tokens [slug]) (join "/"))})
+        (update-in [:aliases] #(conj % (join "/" [dir slug]))))))
 
 (defn parse [filename content]
-  (let [category (first (split filename #"\/"))]
-    (-> (fm/parse content)
-        (update-in [:alias] #(conj [] %))
-        (update-in [:content] md/render)
-        (merge {:filename filename
-                :category category})
-        (clojure.set/rename-keys {:date :post-date :alias :aliases})
-        (#(if (= category "blog")
-          (merge % (parse-post filename))
-          (merge % {:path (strip-ext filename)}))))))
+  (-> (fm/parse content)
+      (update-in [:alias] #(conj [] %))
+      (update-in [:content] md/render)
+      (merge {:filename filename
+              :category (first (split filename #"\/"))})
+      (clojure.set/rename-keys {:date :post-date :alias :aliases})
+      (#(if (= (% :category) "blog")
+        (parse-post % filename)
+        (merge % {:path (strip-ext filename)})))))
