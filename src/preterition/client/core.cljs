eb3cd69d145d8d549752a9ae6d37762fbc01b600
Joe Moon <joe@xoxomoon.com>
2015-06-04 21:54:30 -0700

    figwheel init stuff

--- src/preterition/client/core.cljs
+++ src/preterition/client/core.cljs
@@ -13,20 +13,7 @@
 
 ; (enable-console-print!)
 
-(def nav [{:title "about" :href "/#about"}
-          {:title "code" :href "/#code"}
-          {:title "prose" :href "/#prose"}
-          {:title "blog" :href "/blog"}])
-
 (defonce application-state (atom initialState))
-
-(defn- activate-nav-item [category fragment {href :href :as nav-item}]
-  (let [active (and
-                 (not= href "/")
-                 (or
-                   (= (str "/" category) href)
-                   (= (str "/" fragment) href)))]
-    (assoc nav-item :active active)))
 
 (defn init [state-atom]
   (let [root (if js/document (.getElementById js/document "main"))]
@@ -35,7 +22,7 @@
         (let [{:keys [category path fragment type] :as route} (<! router)]
           (swap! state-atom assoc
                  :event-type type
-                 :nav-data (map (partial activate-nav-item category fragment) nav)
+                 :nav-data (get-nav category fragment)
                  :route nil)
           (if-not (cached? route)
             (swap! state-atom assoc :loading true))
@@ -44,41 +31,47 @@
                  :route {:data (<! (get-route-data {:category category :path path}))
                          :category category
                          :path path
-                         :fragment fragment}))))
+                         :fragment fragment}))))))
+(defonce once
+  (do
+    (if-let [s @application-state]
+      (init application-state)
+      (let [loc (.-location js/document)
+            pathname (.-pathname loc)
+            fragment (.-hash loc)
+            {:keys [category path]} (parse-url-path pathname)]
+        (take!
+          (get-route-data {:category category :path path})
+          (fn [data]
+            (swap! application-state assoc
+                   :loading false
+                   :nav-data (get-nav category fragment)
+                   :route {:data data
+                           :category category
+                           :path path})
+            (init application-state)))))
 
     ; render initially so client can take over
     ; and scroll hooks on index can start
-    (let [state @state-atom
-          {:keys [category path]} (-> state :route)]
-      (q/render (Main state) root)
-      (if (every? clojure.string/blank? [category path])
-        (scroll-to-fragment (-> js/location .-hash))))
+    (when-let [root (if js/document (.getElementById js/document "main"))]
+      (let [state @application-state
+            {:keys [category path]} (-> state :route)]
+        (q/render (Main state) root)
+        (if (every? clojure.string/blank? [category path])
+          (scroll-to-fragment (-> js/location .-hash))))
 
-    (add-watch state-atom :watcher
-      (fn [key atom old-state new-state]
-        (q/render (Main new-state) root)
-        (when-let [route (new-state :route)]
-          (set-title! route)
-          (if (= (new-state :event-type) :click)
-            (scroll-to-fragment (route :fragment))))))
-    (stop)
-    (start)))
+      (add-watch application-state :watcher
+                 (fn [key atom old-state new-state]
+                   (q/render (Main new-state) root)
+                   (when-let [route (new-state :route)]
+                     (set-title! route)
+                     (if (= (new-state :event-type) :click)
+                       (scroll-to-fragment (route :fragment))))))
 
-(defonce go (if-let [s @application-state]
-              (init application-state)
-              (let [loc (.-location js/document)
-                    pathname (.-pathname loc)
-                    fragment (.-hash loc)
-                    {:keys [category path]} (parse-url-path pathname)]
-                (take! (get-route-data {:category category :path path})
-                       (fn [data]
-                         (swap! application-state assoc
-                                :loading false
-                                :nav-data (get-nav category fragment)
-                                :route {:data data
-                                        :category category
-                                        :path path})
-                         (init application-state))))))
+      (start))))
 
 (defn on-jsload []
-  (prn "load"))
+  (do
+    (init application-state)
+    (stop)
+    (start)))
